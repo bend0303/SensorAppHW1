8ca5dd1e75b0591bbb11bb516f307a862766aeeb
orguz <orguz100@gmail.com>
2013-03-17 12:16:54 +0200

    Ready

--- SensorsApp/gen/il/co/shenkar/sensorsapp/R.java
+++ SensorsApp/gen/il/co/shenkar/sensorsapp/R.java
@@ -14,19 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int QR_Scan_Sensor_ID=0x7f070000;
-        public static final int QR_Scan_Sensor_TEXT_ID=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int button3=0x7f070004;
-        public static final int button4=0x7f070006;
-        public static final int button5=0x7f070008;
-        public static final int button6=0x7f07000a;
-        public static final int editText1=0x7f07000b;
-        public static final int menu_settings=0x7f07000c;
-        public static final int textView2=0x7f070003;
-        public static final int textView3=0x7f070005;
-        public static final int textView4=0x7f070007;
-        public static final int textView5=0x7f070009;
+        public static final int Azimuth_Sensor_ID=0x7f070005;
+        public static final int Azimuth_Sensor_Text_ID=0x7f070006;
+        public static final int Light_Sensor_ID=0x7f070007;
+        public static final int Light_Sensor_Text_ID=0x7f070008;
+        public static final int Linear_Acc_Sensor_ID=0x7f070003;
+        public static final int Linear_Acc_Sensor_Text_ID=0x7f070004;
+        public static final int Pressure_Sensor_ID=0x7f070009;
+        public static final int Pressure_Sensor_Text_ID=0x7f07000a;
+        public static final int QR_Generate_ID=0x7f070000;
+        public static final int QR_Scan_Sensor_ID=0x7f070002;
+        public static final int QR_Text_ID=0x7f070001;
+        public static final int menu_settings=0x7f07000b;
     }
     public static final class layout {
         public static final int activity_sensors_view=0x7f030000;
@@ -35,13 +34,18 @@ public final class R {
         public static final int activity_sensors_view=0x7f060000;
     }
     public static final class string {
-        public static final int No_Value_Text=0x7f040003;
+        public static final int Azimuth=0x7f040009;
+        public static final int Light=0x7f04000a;
+        public static final int Linear_Acc=0x7f040008;
+        public static final int No_Value_Text=0x7f040004;
+        public static final int Pressure=0x7f04000b;
+        public static final int QR_Generate_Sensor=0x7f040003;
         public static final int QR_Scan_Sensor=0x7f040002;
-        public static final int Text_Field_Hint=0x7f040004;
+        public static final int Text_Field_Hint=0x7f040005;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040005;
+        public static final int hello_world=0x7f040006;
         public static final int menu_settings=0x7f040001;
-        public static final int title_activity_sensors_view=0x7f040006;
+        public static final int title_activity_sensors_view=0x7f040007;
     }
     public static final class style {
         /** 
--- SensorsApp/src/il/co/shenkar/sensorsapp/activities/SensorsViewActivity.java
+++ SensorsApp/src/il/co/shenkar/sensorsapp/activities/SensorsViewActivity.java
@@ -3,6 +3,9 @@ package il.co.shenkar.sensorsapp.activities;
 import il.co.shenkar.sensorsapp.R;
 import il.co.shenkar.sensorsapp.qr.IntentIntegrator;
 import il.co.shenkar.sensorsapp.qr.IntentResult;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
@@ -12,21 +15,85 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SensorsViewActivity extends Activity {
-	private Button QRScanButton;
-	private TextView QRScanText;
-	//rivate SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+public class SensorsViewActivity extends Activity implements SensorEventListener{
+	public static final int QR_REQUEST_CODE = 0x0000c0de;
+	private Button QRScanButton,QRGenerateButton,LinearAccButton,AzimuthButton,LightButton,pressureButton;
+	private TextView LinearAccText,AzimuthText,LightText,pressureText;
+	private EditText QRGenerateText;
+	private SensorManager sensorManager;
+	private Sensor motionSensor,magSensor,lightSensor,pressureSensor;
+	
 	private IntentIntegrator integrator = new IntentIntegrator(this);
-
+	private float[] mAcc = new float[3];
+	private float[] mOrientation = new float[3];
+	private float[] Rmat = new float[9];
+	private float[] Imat = new float[9];
+	private float[] mGeoMags = new float[3];
+	private float light,pressure;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sensors_view);
 		getElementsView();
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		
+		motionSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		if (motionSensor == null)
+		{
+			LinearAccText.setText("Motion sensor wasn't found");
+			LinearAccButton.setEnabled(false);
+		}
+		magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		if (magSensor == null)
+		{
+			AzimuthText.setText("Magnitude sensor wasn't found");
+			AzimuthButton.setEnabled(false);
+		}
+		lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+		if (lightSensor == null)
+		{
+			LightText.setText("Light sensor wasn't found");
+			LightButton.setEnabled(false);
+		}
+		pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+		if (pressureSensor == null)
+		{
+			pressureText.setText("Pressure sensor wasn't found");
+			pressureButton.setEnabled(false);
+		}
+		
+		sensorManager.registerListener(this, motionSensor,SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, magSensor,SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, pressureSensor,SensorManager.SENSOR_DELAY_NORMAL);
+		
 		setListeners();
 
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorManager.registerListener(this, motionSensor,SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, magSensor,SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, pressureSensor,SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onResume();
+		sensorManager.unregisterListener(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(this);
 	}
 
 	private void setListeners() {
@@ -39,18 +106,81 @@ public class SensorsViewActivity extends Activity {
 			}
 		});
 
+		QRGenerateButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				integrator.shareText(QRGenerateText.getText().toString());
+
+			}
+		});
+
+		LinearAccButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				LinearAccText.setText("X: "+ mAcc[0] +
+						"\nY: "+ mAcc[1] +
+						"\nZ: " + mAcc[2]);
+			}
+		});
+		
+		AzimuthButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				SensorManager.getRotationMatrix(Rmat, Imat, mAcc, mGeoMags);
+				SensorManager.getOrientation(Rmat, mOrientation);
+				AzimuthText.setText("Azimuth: "+ mOrientation[0]);
+			}
+		});
+		
+		LightButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				LightText.setText(Float.toString(light));
+			}
+		});
+		
+		pressureButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				pressureText.setText(Float.toString(pressure));
+			}
+		});
+
 	}
 
 	private void getElementsView() {
 		QRScanButton = (Button) findViewById(R.id.QR_Scan_Sensor_ID);
-		QRScanText = (TextView) findViewById(R.id.QR_Scan_Sensor_TEXT_ID);
+		QRGenerateButton = (Button) findViewById(R.id.QR_Generate_ID);
+		QRGenerateText = (EditText) findViewById(R.id.QR_Text_ID);
+		LinearAccButton = (Button) findViewById(R.id.Linear_Acc_Sensor_ID);
+		LinearAccText = (TextView) findViewById(R.id.Linear_Acc_Sensor_Text_ID);
+		AzimuthButton = (Button) findViewById(R.id.Azimuth_Sensor_ID);
+		AzimuthText = (TextView) findViewById(R.id.Azimuth_Sensor_Text_ID);
+		LightButton = (Button) findViewById(R.id.Light_Sensor_ID);
+		LightText = (TextView) findViewById(R.id.Light_Sensor_Text_ID);
+		pressureButton = (Button) findViewById(R.id.Pressure_Sensor_ID);
+		pressureText = (TextView) findViewById(R.id.Pressure_Sensor_Text_ID);
 	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-		if (scanResult != null) {
-			QRScanText.setText(scanResult.getContents());
+		if (scanResult != null){
+			switch (requestCode) {
+			case QR_REQUEST_CODE:{
+				scanResult.getContents();
+				break;
+			}
+
+			default:
+				break;
+			}
 		}
+
 	}
 
 	@Override
@@ -68,4 +198,30 @@ public class SensorsViewActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		switch (event.sensor.getType()) {
+		case Sensor.TYPE_ACCELEROMETER:
+			System.arraycopy(event.values, 0, mAcc, 0, 3);
+			break;
+		case Sensor.TYPE_MAGNETIC_FIELD:
+            System.arraycopy(event.values, 0, mGeoMags, 0, 3);
+            break;
+		case Sensor.TYPE_LIGHT:
+			light = event.values[0];
+            break;         
+		case Sensor.TYPE_PRESSURE:
+			pressure = event.values[0];
+            break;   
+            
+		default:
+			return;
+		}
+	}
 }
